2b5bb7a67da0d7aee96ab1c492db01174b9526c7
algoshade <123117355+niishaparashar@users.noreply.github.com>
2025-12-07 19:21:19 +0530

    Create LongestCommonPrefix.cpp

---
+++ arrays/LongestCommonPrefix.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty()) return "";
+
+    sort(strs.begin(), strs.end());
+
+    string first = strs[0];
+    string last  = strs.back();
+
+    int i = 0;
+    while (i < first.size() && i < last.size() && first[i] == last[i]) {
+        i++;
+    }
+
+    return first.substr(0, i);
+    }
+};
